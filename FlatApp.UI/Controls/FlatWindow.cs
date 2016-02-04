43581b205f4e1ff353e09618bf6469fde1020e50
raysonxin <xindachao@gmail.com>
2016-02-04 17:18:16 +0800

    modify color.xaml

--- FlatApp.UI/Controls/FlatWindow.cs
+++ FlatApp.UI/Controls/FlatWindow.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FlatApp.UI.Helper;
 
 namespace FlatApp.UI.Controls
 {
@@ -49,6 +50,62 @@ namespace FlatApp.UI.Controls
         static FlatWindow()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(FlatWindow), new FrameworkPropertyMetadata(typeof(FlatWindow)));
+        }
+
+        #region CaptionHeight 标题栏高度
+
+        public static readonly DependencyProperty CaptionHeightProperty =
+            DependencyProperty.Register("CaptionHeight", typeof(double), typeof(FlatWindow), new PropertyMetadata(26D));
+
+        /// <summary>
+        /// 标题高度
+        /// </summary>
+        public double CaptionHeight
+        {
+            get { return (double)GetValue(CaptionHeightProperty); }
+            set
+            {
+                SetValue(CaptionHeightProperty, value);
+                //this._WC.CaptionHeight = value;
+            }
+        }
+
+        #endregion
+
+        public FlatWindow()
+        {
+            this.Style = this.FindResource("DefaultFlatWindowStyle") as Style;
+            //bind command
+            this.BindCommand(SystemCommands.CloseWindowCommand, this.CloseCommand_Execute);
+            this.BindCommand(ApplicationCommands.Close, this.CloseCommand_Execute);
+            this.BindCommand(SystemCommands.MaximizeWindowCommand, this.MaxCommand_Execute);
+            this.BindCommand(SystemCommands.MinimizeWindowCommand, this.MinCommand_Execute);
+        }
+
+        private void CloseCommand_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            SystemCommands.CloseWindow(this);
+        }
+
+        private void MaxCommand_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.WindowState = this.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            e.Handled = true;
+        }
+
+        private void MinCommand_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+            e.Handled = true;
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
         }
     }
 }
---
+++ FlatApp.UI/Helper/Extends.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FlatApp.UI.Helper
+{
+    public static class Extends
+    {
+        #region BindCommand
+
+        /// <summary>
+        /// 绑定命令和命令事件到宿主UI
+        /// </summary>
+        public static void BindCommand(this UIElement @ui, ICommand com, Action<object, ExecutedRoutedEventArgs> call)
+        {
+            var bind = new CommandBinding(com);
+            bind.Executed += new ExecutedRoutedEventHandler(call);
+            @ui.CommandBindings.Add(bind);
+        }
+
+        ///// <summary>
+        ///// 绑定RelayCommand命令到宿主UI
+        ///// </summary>
+        //public static void BindCommand(this UIElement @ui, RelayCommand<object> com)
+        //{
+        //    var bind = new CommandBinding(com);
+        //    bind.Executed += delegate(object sender, ExecutedRoutedEventArgs e)
+        //    {
+        //        com.ExecuteCommand(e.Parameter);
+        //    };
+        //    @ui.CommandBindings.Add(bind);
+        //}
+
+        #endregion
+
+
+    }
+}
